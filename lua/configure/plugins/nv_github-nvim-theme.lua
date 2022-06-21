74946b3c87314a9022341dd1f562ead463f3a8c3
yi-ge <a@wyr.me>
2022-06-21 20:23:06 +0800

    feat: 根据白天黑夜自动切换主题

--- lua/configure/plugins/nv_github-nvim-theme.lua
+++ lua/configure/plugins/nv_github-nvim-theme.lua
@@ -6,7 +6,38 @@ local M = {}
 
 function M.before() end
 
+
+--获取当日时间戳0点
+local function getTodayTimeStamp()
+  local cDateCurrectTime = os.date("*t")
+  local cDateTodayTime = os.time({year=cDateCurrectTime.year, month=cDateCurrectTime.month, day=cDateCurrectTime.day, hour=0,min=0,sec=0})
+  return cDateTodayTime
+end
+
+--格式化时间为时间戳
+function FormatTime(lastDate,day,hour)
+	local dayTimestamp = 24* 60* 60
+	lastDate = lastDate + dayTimestamp  *  day
+	local date = os.date("*t", time)
+	--这里返回的是你指定的时间点的时间戳
+	return os.time({year=date.year, month=date.month, day=date.day, hour=hour, minute = date.minute, second = date.second})
+end
+
 function M.load()
+    local theme_style = ""
+    -- mode
+    --获取当日时间戳0点
+    local cDateTodayTime = getTodayTimeStamp()
+    --当前时间戳
+    local cDateCurrectTime = os.time()
+    local cDateTodayTime_8 = FormatTime(cDateTodayTime,0,8)
+    local cDateTodayTime_19 = FormatTime(cDateTodayTime,0,19)
+    if cDateTodayTime_8 < cDateCurrectTime and cDateCurrectTime < cDateTodayTime_19 then
+        theme_style = "light"
+    else
+        theme_style = "dark"
+    end
+
     local ok, m = pcall(require, "github-theme")
     if not ok then
         return
@@ -16,7 +47,7 @@ function M.load()
     M.github_theme.setup({
         -- Theme style to use
         -- theme_style = "dark",
-        theme_style = "light",
+        theme_style = theme_style,
         -- Set the style of treesitter
         comment_style = "NONE",
         function_style = "NONE",
--- lua/configure/plugins/nv_vscode.lua
+++ lua/configure/plugins/nv_vscode.lua
@@ -6,10 +6,36 @@ local M = {}
 
 function M.before() end
 
+
+--获取当日时间戳0点
+local function getTodayTimeStamp()
+  local cDateCurrectTime = os.date("*t")
+  local cDateTodayTime = os.time({year=cDateCurrectTime.year, month=cDateCurrectTime.month, day=cDateCurrectTime.day, hour=0,min=0,sec=0})
+  return cDateTodayTime
+end
+
+--格式化时间为时间戳
+function FormatTime(lastDate,day,hour)
+	local dayTimestamp = 24* 60* 60
+	lastDate = lastDate + dayTimestamp  *  day
+	local date = os.date("*t", time)
+	--这里返回的是你指定的时间点的时间戳
+	return os.time({year=date.year, month=date.month, day=date.day, hour=hour, minute = date.minute, second = date.second})
+end
+
 function M.load()
     -- mode
-    vim.g.vscode_style = "dark"
-    -- vim.g.vscode_style = "light"
+    --获取当日时间戳0点
+    local cDateTodayTime = getTodayTimeStamp()
+    --当前时间戳
+    local cDateCurrectTime = os.time()
+    local cDateTodayTime_8 = FormatTime(cDateTodayTime,0,8)
+    local cDateTodayTime_19 = FormatTime(cDateTodayTime,0,19)
+    if cDateTodayTime_8 < cDateCurrectTime and cDateCurrectTime < cDateTodayTime_19 then
+        vim.g.vscode_style = "light"
+    else
+        vim.g.vscode_style = "dark"
+    end
     -- transparent background
     vim.g.vscode_transparent = options.transparent_background
     -- Enable italic comment
